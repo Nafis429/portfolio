bf2345aab8965403373f79e37da7279f8bfa355f
Shahriar Nafis <nafis449@Shahriars-MacBook-Air.local>
2023-11-26 00:13:00 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var tablinks=document.getElementsByClassName("tab-links");
+var tabcontents=document.getElementsByClassName("tab-contents");
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+var sidemenu=document.getElementById("sidemenu");
+function openMenu(){
+    sidemenu.style.right="0";
+}
+function closeMenu(){
+    sidemenu.style.right="-200px";
+}
